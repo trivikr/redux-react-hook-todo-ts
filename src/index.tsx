e9bf21a70fe1bb41a99db8e13be8549bbbc59ca5
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-21 16:07:37 -0700

    Rename rootReducer -> index

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from "./components/App";
-import rootReducer from "./store/rootReducer";
+import rootReducer from "./store";
 
 const store = createStore(rootReducer);
 
