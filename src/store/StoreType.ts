4c105df9bc96b866b23a7669c206c1ff1cd08ada
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-21 16:06:48 -0700

    Remove unnecessary files

--- src/store/StoreType.ts
+++
@@ -1,7 +0,0 @@
-import { TodoType } from "./todos/types";
-import { VisibilityFilterType } from "./visibilityFilter/types";
-
-export interface StoreType {
-  todos: TodoType[];
-  visibilityFilter: VisibilityFilterType;
-}
