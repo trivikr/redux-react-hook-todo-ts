66152799db089523b1d896a15d3e0521235213d4
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-25 12:50:27 -0700

    Added comment to disable @typescript-eslint/prefer-interface
    Reason: VisibilityFilterAction is unlikely to be extended
    Rule doc: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/prefer-interface.md

--- src/store/visibilityFilter/types.ts
+++ src/store/visibilityFilter/types.ts
@@ -8,6 +8,7 @@ export enum VisibilityFilterActionType {
   SET_VISIBILITY_FILTER = "@@visibilityFilter/SET_VISIBILITY_FILTER"
 }
 
+// eslint-disable-next-line @typescript-eslint/prefer-interface
 export type VisibilityFilterAction = {
   type: VisibilityFilterActionType.SET_VISIBILITY_FILTER;
   filter: VisibilityFilterType;
