af4f33742da082b4191bcec3f296aaffd85bc936
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-25 12:05:13 -0700

    Adding path to project's tsconfig.json to use rules which require type information
    Doc: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 // Prefering .eslintrc.js over a JSON file, as it lets you leave comments in your configuration!
 module.exports = {
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
   plugins: ["@typescript-eslint"],
   extends: ["plugin:@typescript-eslint/recommended"],
   rules: {
