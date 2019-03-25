a7bb02486e5486bd7061586b35a04c410cc88bfa
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-25 10:26:10 -0700

    Added dependency on typescript-eslint for linting
    Blog post: https://javascriptplayground.com/typescript-eslint/

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+// Prefering .eslintrc.js over a JSON file, as it lets you leave comments in your configuration!
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended"]
+};
