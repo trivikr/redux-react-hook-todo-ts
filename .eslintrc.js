2ebe49641e853e726ad83bf83dedd09b4496e11d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-25 12:14:06 -0700

    Add typescript-eslint to use ESLint (#1)
    * Experimenting with typescript-eslint to use ESLint for TypeScript project
    
    Limiting TypeScript to minor version 3.3.x as newer versions might have breaking versions
    Doc: https://github.com/typescript-eslint/typescript-eslint#supported-typescript-version
    
    * Added dependency on typescript-eslint for linting
    
    Blog post: https://javascriptplayground.com/typescript-eslint/
    
    * Removing eslint dependency as CRA adds it through react-scripts
    
    * Disable @typescript-eslint/no-unused-vars bec of bug with typescript-eslint
    
    Refs: https://github.com/typescript-eslint/typescript-eslint/issues/363
    
    * Adding path to project's tsconfig.json to use rules which require type information
    
    Doc: https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/parser#configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// Prefering .eslintrc.js over a JSON file, as it lets you leave comments in your configuration!
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off"
+  }
+};
