3878cad92f63210ff97ab881134f64a3a62ea17d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-25 12:45:58 -0700

    Disable "@typescript-eslint/explicit-function-return-type"
    Refs: https://github.com/typescript-eslint/typescript-eslint/pull/260
    ToDo: Enable when fix is available

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "prettier/@typescript-eslint"
   ],
   rules: {
-    "@typescript-eslint/no-unused-vars": "off"
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off"
   }
 };
