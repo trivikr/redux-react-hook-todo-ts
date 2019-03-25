bbc35d9388bb69b083134de2a62cbba9d0e9c421
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2019-03-25 11:59:35 -0700

    Disable @typescript-eslint/no-unused-vars bec of bug with typescript-eslint
    Refs: https://github.com/typescript-eslint/typescript-eslint/issues/363

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,8 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["plugin:@typescript-eslint/recommended"]
+  extends: ["plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off"
+  }
 };
